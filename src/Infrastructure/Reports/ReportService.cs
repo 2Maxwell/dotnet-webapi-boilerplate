120427da8a41b5d1fc52de72ff1114897d75b642
Orlando Malo <orlandomalo0@gmail.com>
2023-06-23 23:14:50 +0200

    added interface and packages

---
+++ src/Infrastructure/Reports/ReportService.cs
@@ -0,0 +1,11 @@
+﻿using FSH.WebApi.Application.ReportsContract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSH.WebApi.Infrastructure.Reports;
+public class ReportService : IReportService
+{
+}
